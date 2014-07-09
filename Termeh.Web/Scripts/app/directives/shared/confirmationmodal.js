2d151d771d3eb0fd79e5bf775e005ae2f252d7e3
mfahim <mfahim@gmail.com>
2014-07-09 23:57:03 +1000

    Some UI changes to modal dialogbox, apply some css classes

--- Termeh.Web/Scripts/app/directives/shared/confirmationmodal.js
+++ Termeh.Web/Scripts/app/directives/shared/confirmationmodal.js
@@ -3,13 +3,15 @@
     var app = angular.module("termeh");
     app.directive('confirmationModal', function () {
         return {
+            restrict: 'E',
             scope: {
                 approve: '&onApprove',
                 deny: '&onDeny',
-                closable: '=closable'
+                closable: '=closable',
+                modalTitle: '=modalTitle'
             },
             template: '<a href="javascript:;"><i class="trash icon"></i></a><div class="ui basic modal" id="modal">' +
-                        '<div class="header">Modal Title</div><div class="content"><div class="left">Content can appear on left</div><div class="right">' +
+                        '<div class="header">{{modalTitle}}</div><div class="content"><div class="left"> {{modalTitle}} Content can appear on left</div><div class="right">' +
                         'Content can appear on right</div></div><div class="actions"><div class="two fluid ui buttons"><div ng-click="closeModal();deny();" class="ui negative labeled icon button">No</div>' +
                         '<div class="ui positive labeled icon button button" ng-click="approve();">Yes</div></div></div></div>',
             link: function (scope, element, attrs, ngModelCtrl) {
