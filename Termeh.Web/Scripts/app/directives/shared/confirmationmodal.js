e57a201179bec47150c69fc9d4b12971debb4a32
mfahim <mfahim@gmail.com>
2014-07-17 23:49:05 +1000

    Apply confirmation-box to delete jobAttachment(s)

--- Termeh.Web/Scripts/app/directives/shared/confirmationmodal.js
+++ Termeh.Web/Scripts/app/directives/shared/confirmationmodal.js
@@ -10,9 +10,9 @@
                 closable: '=closable',
                 title: '=modalTitle'
             },
-            template: '<a href="javascript:;"><i class="trash icon"></i></a><div class="ui small modal" id="modal">' +
-                        '<div class="header">{{title}}</div><div class="content"><div class="left"> Content can appear on left</div><div class="right">' +
-                        'Content can appear on right</div></div><div class="actions"><div class="two fluid ui buttons"><div ng-click="closeModal();deny();" class="ui negative labeled icon button">No</div>' +
+            template: '<a href="javascript:;"><i class="circular red trash icon"></i></a><div class="ui small modal" id="modal">' +
+                        '<div class="header">{{title}}</div><div class="content"><div> Are you sure you want to delete this item?' +
+                        '</div></div><div class="actions"><div class="two fluid ui buttons"><div ng-click="closeModal();deny();" class="ui negative labeled icon button">No</div>' +
                         '<div class="ui positive labeled icon button button" ng-click="approve();">Yes</div></div></div></div>',
             link: function (scope, element, attrs, ngModelCtrl) {
 
