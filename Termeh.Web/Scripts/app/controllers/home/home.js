540c3bab0765fdf2b8b1578efd20dd0ccb7bbf5f
mfahim <mfahim@gmail.com>
2014-07-24 23:50:39 +1000

    Change css classes in modal dialogbox, refactor home controller in js

--- Termeh.Web/Scripts/app/controllers/home/home.js
+++ Termeh.Web/Scripts/app/controllers/home/home.js
@@ -3,12 +3,6 @@
     var app = angular.module("termeh.controllers");
 
     var homeController = function ($scope) {
-
-        init();
-
-        function init() {
-            $scope.greeting = 'salam!';
-        }
     };
     homeController.$inject = ['$scope'];
 
--- Termeh.Web/Scripts/app/directives/shared/confirmationmodal.js
+++ Termeh.Web/Scripts/app/directives/shared/confirmationmodal.js
@@ -14,8 +14,8 @@
             },
             template: '<a href="javascript:;"><i class="circular red trash icon"></i></a><div class="ui small modal" id="modal">' +
                         '<div class="header"> {{title}}</div><div class="content"><div> Are you sure you want to delete this item?' +
-                        '</div></div><div class="actions"><div class="two fluid ui buttons"><div ng-click="closeModal();deny();" class="ui negative labeled icon button">No</div>' +
-                        '<div class="ui positive labeled icon button button" ng-click="approve();">Yes</div></div></div></div>',
+                        '</div></div><div class="actions"><div ng-click="closeModal();deny();" class="ui negative button">No</div>' +
+                        '<div class="ui positive right labeled icon button" ng-click="approve();">Yes <i class="checkmark icon"></i></div></div></div>',
             link: function (scope, element, attrs, ngModelCtrl) {
 
                 element.on('click', function (e) {
