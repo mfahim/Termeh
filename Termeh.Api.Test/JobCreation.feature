2c831445bc583559fb56dbdc0e0f5003d741360b
mfahim <mfahim@gmail.com>
2014-08-06 23:42:46 +1000

    Introducing Termeh.Api.Test

---
+++ Termeh.Api.Test/JobCreation.feature
@@ -0,0 +1,9 @@
+﻿Feature: JobCreation
+	In order to create print job
+	As a customer representative
+	I want to be able to create jobs
+
+@mytag
+Scenario: Add two numbers
+	When I press newJob
+	Then the result should be a new job on Termeh
---
+++ Termeh.Api.Test/JobCreationSteps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using JobTrack.Api.Data.Commands;
+using TechTalk.SpecFlow;
+
+namespace Termeh.Api.Test
+{
+    [Binding]
+    public class JobCreationSteps
+    {
+        public JobCreationSteps()
+        {
+        }
+
+        [When(@"I press newJob")]
+        public void WhenIPressNewJob()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be a new job on Termeh")]
+        public void ThenTheResultShouldBeANewJobOnTermeh()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
