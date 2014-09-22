eada79229256128f24867c5df40ba8b30ccac400
mfahim <mfahim@gmail.com>
2014-09-22 23:34:40 +1000

    Adding new unit tests for getting all users

---
+++ Termeh.Api.Test/UnitTests/User/When_Getting_ListOfUsers.cs
@@ -0,0 +1,24 @@
+using JobTrack.Api.Controllers;
+using JobTrack.Api.Data.Queries.User;
+using NSubstitute;
+using NUnit.Framework;
+using ShortBus;
+
+namespace Termeh.Api.Test.UnitTests.User
+{
+    [TestFixture]
+    public class When_Getting_ListOfUsers
+    {
+        [Test]
+        public void Should_Call_ShowUsersQuery_With_GetAll()
+        {
+            var mediatorMock = Substitute.For<IMediator>();
+
+            var userCtrl = Substitute.For<UserController>(mediatorMock);
+
+            userCtrl.Get();
+            var arg = Arg.Any<ShowUsersQuery>();
+            userCtrl.Received().Query(arg);
+        }
+    }
+}
